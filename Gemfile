4f362e21b74811b9ddd8b0c944fccd2106cd5824
Pete Hawkins <pete@phawk.co.uk>
2015-11-09 16:58:08 +0000

    initial commit for postgis

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+ruby '2.2.2'
+
+gem 'rails', '4.2.4'
+gem 'pg'
+gem 'activerecord-postgis-adapter'
+gem 'geocoder'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'byebug'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
---
+++ app/models/store.rb
@@ -0,0 +1,28 @@
+class Store < ActiveRecord::Base
+  GEO_FACTORY = RGeo::Geographic.spherical_factory(srid: 4326)
+
+  geocoded_by :street_address do |record, results|
+    result = results.first
+
+    fail "Geocoding failed" if result.nil?
+
+    record.street_address = result.address
+    record.lonlat = GEO_FACTORY.point(result.latitude, result.longitude)
+  end
+
+  after_validation :geocode, if: ->(obj){ obj.street_address.present? and obj.street_address_changed? }
+
+  # e.g. Store.nearby(54.574997, -5.893822).pluck(:name)
+  def self.nearby(lat, lon, distance: 5000)
+    Store.where("ST_Distance(lonlat, ST_GeographyFromText('SRID=4326;POINT(:lat :lon)')) < :distance", lat: lat, lon: lon, distance: distance)
+  end
+end
+
+# Nearby back arse of carrickfergus
+# Store.nearby(54.731510 , -5.772972, distance: 20000).pluck(:name)
+
+# Nearby castlereagh
+# Store.nearby(54.574997, -5.893822).pluck(:name)
+
+# Nearby titanic qt
+# Store.nearby(54.604855 , -5.911374).pluck(:name)
